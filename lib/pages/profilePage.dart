0ec841596a86a0e2c7d72a98811ac0f03ba6d15c
“Milolikaxx” <nusitday106773@gmail.com>
2023-10-29 17:45:16 +0700

    profile post

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -15,24 +15,26 @@ class _ProfilePageState extends State<ProfilePage> {
   Timgs? timgs;
   bool isLoading = false;
 
-  void initState(){
+  @override
+  void initState() {
     super.initState();
     String? title;
     isLoading = true;
     title = 'Loading products...';
     timgs = Timgs();
 
-    Services.getTimgs().then((timgsFromServer){
+    Services.getTimgs().then((timgsFromServer) {
       setState(() {
         timgs = timgsFromServer;
         isLoading = false;
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 250, 243, 243),
+      backgroundColor: const Color.fromARGB(0, 24, 24, 24),
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: Image.network(
@@ -41,145 +43,168 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       body: SingleChildScrollView(
         child: isLoading
-        ? const Center(
-            child: CircularProgressIndicator(),
-          ) 
-          : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            const CircleAvatar(
-              radius: 50.0,
-              backgroundImage: NetworkImage(
-                  'https://i.pinimg.com/564x/26/dc/3c/26dc3c8e0b156e8eeeaf75964281058f.jpg'),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'LnwCatCat2000',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 5),
-            const Text(
-              'รีวิวทุกอย่าง',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '486',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 60),
-                Text(
-                  '397',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Follower'),
-                SizedBox(width: 30),
-                Text('Following'),
-              ],
-            ),
-            const SizedBox(height: 10,),
-            OutlinedButton(onPressed: () {
-              
-            }, 
-             style: ButtonStyle(
-                minimumSize:
-                    MaterialStateProperty.all<Size>(const Size(150, 50)),
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  const CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: NetworkImage(
+                        'https://i.pinimg.com/564x/26/dc/3c/26dc3c8e0b156e8eeeaf75964281058f.jpg'),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'LnwCatCat2000',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'รีวิวทุกอย่าง',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '486',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(width: 60),
+                      Text(
+                        '397',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Follower'),
+                      SizedBox(width: 30),
+                      Text('Following'),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  OutlinedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all<Size>(
+                            const Size(150, 50)),
+                      ),
+                      child: const Text('Edit',
+                          style: TextStyle(fontSize: 18, color: Colors.black))),
+                  const SizedBox(height: 5,),
+                  post()
+                ],
               ),
-            child: const Text('Edit',style: TextStyle(fontSize: 18, color: Colors.black ))
-            ),
-            post()
-          ],
-        ),
       ),
     );
   }
 
   Widget post() {
+    var liked;
     return Card(
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+       shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
       ),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-           const Row(
-            children: [
-               SizedBox(
-                width: 20,
-              ),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage(
-                    'https://i.pinimg.com/564x/26/dc/3c/26dc3c8e0b156e8eeeaf75964281058f.jpg'),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'LnwCatCat2000',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10,),
-          // Image.network(
-          //   'https://media.istockphoto.com/id/1253629795/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/tteokbokki-%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%84%E0%B8%82%E0%B9%88%E0%B9%83%E0%B8%99%E0%B8%8A%E0%B8%B2%E0%B8%A1%E0%B8%AA%E0%B8%B5%E0%B9%80%E0%B8%97%E0%B8%B2%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B8%81%E0%B8%A3%E0%B8%B5%E0%B8%95-tteok-bokki-%E0%B9%80%E0%B8%9B%E0%B9%87%E0%B8%99%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%81%E0%B8%B2%E0%B8%AB%E0%B8%A5%E0%B8%B5%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%A1%E0%B8%B5%E0%B9%80%E0%B8%84%E0%B9%89%E0%B8%81%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%A7.jpg?s=612x612&w=0&k=20&c=AQCLOre-Y9-ciPMTrb4ARYPwEecNcRd8aH8hwvigR8o=',
-          //   fit: BoxFit.cover,
-          // ),
-          Image.memory(
-            base64Decode(timgs!.timgs[3].img),
-            fit: BoxFit.cover,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
               children: [
-                SizedBox(height: 5),
-                Text(
-                  'Tteokbokki A popular Korean street food made with chewy rice cakes in a spicy sauce.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                      "https://i.pinimg.com/564x/26/dc/3c/26dc3c8e0b156e8eeeaf75964281058f.jpg"),
+                  backgroundColor: Colors.transparent,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "LnwCatCat2000",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    Text("1h")
+                  ],
+                ),
+                const Spacer(),
+                PopupMenuButton(
+                      itemBuilder: (context) => [
+                          const PopupMenuItem(
+                          child: Row(
+                            children: [
+                                Text("ลบ"),
+
+                            ],
+                          ),
+                        ),
+                        // const PopupMenuItem(
+                        //   child: Row(
+                        //     children: [
+                        //         Text("แก้ไข"),
+
+                        //     ],
+                        //   ),
+                        // )
+                        ],
+                        child:  const Icon(Icons.more_vert),
                 ),
               ],
             ),
-          ),
-         Row(
-              children: [
-                 const SizedBox(
-                width: 20,
-              ),
-                 IconButton(
-                    icon: const Icon(Icons.pets ,color: Color(0xFFF8721D),),
+            const SizedBox(
+              height: 5,
+            ),
+            Image.network(
+              "https://p16-sign-sg.lemon8cdn.com/tos-alisg-v-a3e477-sg/oUAABDIgJDA0jXedJnEvF1ebeA6IgQbbr7LTsO~tplv-sdweummd6v-shrink:1080:0:q50.webp?source=seo_feed_list&x-expires=1725386400&x-signature=wvCLLx640nBjuvcyR%2FXz1wISBAY%3D",
+              width: 300,
+              height: 250,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Row(children: [
+                IconButton(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                    constraints: const BoxConstraints(),
                     onPressed: () {
-                      // Do something
                     },
-                 ),
-                  const Text(
-                '500',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
+                    icon: liked == true
+                        ? const Icon(
+                            Icons.pets,
+                            color: Color(0xFFF8721D),
+                          )
+                        : const Icon(
+                            Icons.pets_outlined,
+                            color: Colors.black54,
+                          )),
+                  const SizedBox(width: 8,),
+                  const Text("500",
+                  style: TextStyle(fontSize: 16,),
                 ),
+              ]
               ),
-              ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
